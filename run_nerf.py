5ab53c409ae81ee9850c769d28bc24f205359636
emanuelecaruso <caruso.manu81@gmail.com>
2022-11-09 16:33:54 +0100

    new

--- run_nerf.py
+++ run_nerf.py
@@ -804,6 +804,7 @@ def train():
             with torch.no_grad():
                 rgbs, disps = render_path(render_poses, hwf, K, args.chunk, render_kwargs_test)
             print('Done, saving', rgbs.shape, disps.shape)
+            print(moviebase)
             moviebase = os.path.join(basedir, expname, '{}_spiral_{:06d}_'.format(expname, i))
             imageio.mimwrite(moviebase + 'rgb.mp4', to8b(rgbs), fps=30, quality=8)
             imageio.mimwrite(moviebase + 'disp.mp4', to8b(disps / np.max(disps)), fps=30, quality=8)
