ba521915e8ba9dae18135309fd6f20c8cc38330c
emanuelecaruso <caruso.manu81@gmail.com>
2022-11-09 14:40:13 +0100

    1 iter

--- run_nerf.py
+++ run_nerf.py
@@ -698,7 +698,7 @@ def train():
         rays_rgb = torch.Tensor(rays_rgb).to(device)
 
 
-    N_iters = 1 + 1
+    N_iters = 100 + 1
     print('Begin')
     print('TRAIN views are', i_train)
     print('TEST views are', i_test)
