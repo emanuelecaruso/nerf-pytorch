2b85573ede2ddfdc8fae970344c5ccc47b41df6f
emanuelecaruso <caruso.manu81@gmail.com>
2022-11-14 15:26:25 +0100

    newcomm

---
+++ run_nerf.sh
@@ -0,0 +1,31 @@
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+PYTHONSCRIPT=${SCRIPTPATH}"/run_nerf.py"
+CONFIGSFILE=${SCRIPTPATH}"/configs/lego.txt"
+ENVFILE=${SCRIPTPATH}"/env/bin/activate"
+
+
+# Find env activation file
+#if [ -f "$ENVFILE" ]; then
+#    echo "Environment file found"
+#else 
+#    echo "$FILE does not exist. (Environment not found)"
+#    exit
+#fi
+
+# Find python script
+if [ -f "$ENVFILE" ]; then
+    echo "Python script found"
+else 
+    echo "$FILE does not exist."
+    exit
+fi
+
+# Enter in virtual environment
+#source ${ENVFILE}
+
+# Install requirements
+#pip install -r requirements.txt
+
+# Run nerf
+python ${PYTHONSCRIPT} --config ${CONFIGSFILE}
