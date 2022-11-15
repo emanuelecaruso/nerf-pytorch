83225c6b73020c57515c18e8605ab57f4019bf6a
emanuelecaruso <caruso.manu81@gmail.com>
2022-11-15 09:43:52 +0100

    push from server

---
+++ run_nerf_server.sh
@@ -0,0 +1,26 @@
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+PYTHONSCRIPT=${SCRIPTPATH}"/run_nerf.py"
+CONFIGSFILE=${SCRIPTPATH}"/configs/lego.txt"
+ENVFILE="/home/venvs/torch_1.11.0_ema/bin/activate"
+
+
+# Find env activation file
+if [ -f "$ENVFILE" ]; then
+    echo "Environment file found"
+else 
+    echo "$FILE does not exist. (Environment not found)"
+    exit
+fi
+
+# Find python script
+if [ -f "$ENVFILE" ]; then
+    echo "Python script found"
+else 
+    echo "$FILE does not exist."
+    exit
+fi
+
+# Run nerf
+python3.8 ${PYTHONSCRIPT} --config ${CONFIGSFILE}
